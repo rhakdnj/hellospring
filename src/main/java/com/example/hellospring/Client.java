b9dd179751df6e3dc5475ce9481a93c4a4d284cf
rhakdnj <chungjm0711@gmail.com>
2024-08-19 23:33:49 +0900

    feat: DI Container

--- src/main/java/com/example/hellospring/Client.java
+++ src/main/java/com/example/hellospring/Client.java
@@ -1,12 +1,14 @@
 package com.example.hellospring;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import java.io.IOException;
 import java.math.BigDecimal;
 
 public class Client {
 	public static void main(String[] args) throws IOException {
-		var objectFactory = new ObjectFactory();
-		var paymentService = objectFactory.paymentService();
+		var beanFactory = new AnnotationConfigApplicationContext(ObjectFactory.class);
+		var paymentService = beanFactory.getBean(PaymentService.class);
 
 		var payment = paymentService.prepare(1L, "USD", BigDecimal.valueOf(1000.3));
 		System.out.println(payment);
--- src/main/java/com/example/hellospring/ObjectFactory.java
+++ src/main/java/com/example/hellospring/ObjectFactory.java
@@ -1,10 +1,16 @@
 package com.example.hellospring;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class ObjectFactory {
+	@Bean
 	public PaymentService paymentService() {
 		return new PaymentService(exchangeRateReader());
 	}
 
+	@Bean
 	public ExchangeRateReader exchangeRateReader() {
 		return new WebExchangeRateReader();
 	}
