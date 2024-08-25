000e7974ebd4a9d666ec87e5852e0e48f8b00ba2
rhakdnj <chungjm0711@gmail.com>
2024-08-25 12:48:18 +0900

    feat: EntityManager 방식

--- src/main/java/com/example/hellospring/DataClient.java
+++ src/main/java/com/example/hellospring/DataClient.java
@@ -3,6 +3,9 @@ package com.example.hellospring;
 import com.example.hellospring.order.Order;
 import com.example.hellospring.repository.OrderRepository;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import java.math.BigDecimal;
 
@@ -10,7 +13,23 @@ public class DataClient {
 	public static void main(String[] args) {
 		var beanFactory = new AnnotationConfigApplicationContext(DataConfig.class);
 		var orderRepository = beanFactory.getBean(OrderRepository.class);
+		var transactionManager = beanFactory.getBean(JpaTransactionManager.class);
 
-		orderRepository.save(new Order("1234", BigDecimal.valueOf(1000.3)));
+		try {
+			new TransactionTemplate(transactionManager).execute(status -> {
+				Order order1 = new Order("100", BigDecimal.ONE);
+				orderRepository.save(order1);
+
+				System.out.println(order1);
+
+				Order order2 = new Order("100", BigDecimal.TWO);
+				orderRepository.save(order2);
+
+				return null;
+			});
+
+		} catch (DataIntegrityViolationException e) {
+			System.out.println("주문번호 중복 복구 작업");
+		}
 	}
 }
--- src/main/java/com/example/hellospring/repository/OrderRepository.java
+++ src/main/java/com/example/hellospring/repository/OrderRepository.java
@@ -1,28 +1,14 @@
 package com.example.hellospring.repository;
 
 import com.example.hellospring.order.Order;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.EntityTransaction;
-import lombok.RequiredArgsConstructor;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 
-@RequiredArgsConstructor
 public class OrderRepository {
-	private final EntityManagerFactory emf;
+	@PersistenceContext
+	private EntityManager entityManager;
 
 	public void save(Order order) {
-		var em = emf.createEntityManager();
-		EntityTransaction transaction = em.getTransaction();
-		transaction.begin();
-
-		try {
-			em.persist(order);
-
-			transaction.commit();
-		} catch (RuntimeException e) {
-			if (transaction.isActive()) transaction.rollback();
-			throw e;
-		} finally {
-			if (em.isOpen()) em.close();
-		}
+		entityManager.persist(order);
 	}
 }
